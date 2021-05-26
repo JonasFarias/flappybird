c2084ffa0355336c55914f640b560e35b7049218
Jonas <jhonas.jr@hotmail.com>
2021-05-25 21:02:01 -0300

    Iniciando o projeto do jogo
    criando a janela do game e inserindo a imagem do jogo
    
    Criando um menu inicial, colocando a funcionalidade de iniciar o game

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
+from kivy.uix.screenmanager import ScreenManager, Screen
 
+class Manager(ScreenManager):
+    pass
+class Menu(Screen):
+    pass
+class Game(Screen):
+    pass
+class Player(Image):
+    pass
 class HashBird(App):
-    def build(self):
-        player = Image(source='attack_1.png')
-        layout = FloatLayout()
-        layout.add_widget(player)
-        return layout
+    pass
 
 
 HashBird().run()
